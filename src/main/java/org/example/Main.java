856140e38b9bda9447e6aaa6caba71ba1ed62fb9
Sarmat <xonely@icloud.com>
2023-07-16 13:47:27 +0300

    v1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,55 +6,53 @@ public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
         ExecutorService es = Executors.newFixedThreadPool(3);
-        //region Note
-
-        // Runnable r1 = new Runnable() {
-        //   @Override
-        //    public void run() {
-        //        foo.first(this);
-        //    }
-        // };
-        // es.submit(r1);
-        // Если нужно было сохранить параметры методов Foo принимающие (Runnable r) и потом через wait/notify их синхронизировать
-
-        //endregion
-        es.execute(() -> {
-            foo.first();
-        });
-        es.execute(() -> {
-            try {
-                foo.third();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                foo.first(this);
             }
-        });
-        es.execute(() -> {
-            try {
-                foo.second();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        };
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                foo.second(this);
             }
-        });
+        };
+        Runnable r3 = new Runnable() {
+            @Override
+            public void run() {
+                foo.third(this);
+            }
+        };
+
+        es.execute(r2);
+        es.execute(r3);
+        es.execute(r1);
     }
 }
 
 class Foo {
     private Semaphore semaphoreFirst = new Semaphore(0);
     private Semaphore semaphoreSecond = new Semaphore(0);
-
-    public void first() {
+    public void first(Runnable r) {
         System.out.print("first");
         semaphoreFirst.release();
     }
-
-    public void second() throws InterruptedException {
-        semaphoreFirst.acquire();
+    public void second(Runnable r) {
+        try {
+            semaphoreFirst.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.print("second");
         semaphoreSecond.release();
     }
-
-    public void third() throws InterruptedException {
-        semaphoreSecond.acquire();
+    public void third(Runnable r) {
+        try {
+            semaphoreSecond.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.print("third");
     }
 }
