50c07032529156f56cc2a4e6a892081516316e54
Sarmat <xonely@icloud.com>
2023-07-16 14:20:47 +0300

    v1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,53 +6,39 @@ public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
         ExecutorService es = Executors.newFixedThreadPool(3);
-        Runnable r1 = new Runnable() {
-            @Override
-            public void run() {
-                foo.first(this);
-            }
-        };
-        Runnable r2 = new Runnable() {
-            @Override
-            public void run() {
-                foo.second(this);
-            }
-        };
-        Runnable r3 = new Runnable() {
-            @Override
-            public void run() {
-                foo.third(this);
-            }
-        };
-
-        es.execute(r2);
-        es.execute(r3);
-        es.execute(r1);
+        es.execute(() -> foo.first(new Thread()));
+        es.execute(() -> foo.third(new Thread()));
+        es.execute(() -> foo.second(new Thread()));
     }
 }
 
 class Foo {
     private Semaphore semaphoreFirst = new Semaphore(0);
     private Semaphore semaphoreSecond = new Semaphore(0);
+
     public void first(Runnable r) {
         System.out.print("first");
         semaphoreFirst.release();
     }
+
     public void second(Runnable r) {
         try {
             semaphoreFirst.acquire();
+            System.out.print("second");
+            semaphoreSecond.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.print("second");
-        semaphoreSecond.release();
+
     }
+
     public void third(Runnable r) {
         try {
             semaphoreSecond.acquire();
+            System.out.print("third");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.print("third");
+
     }
 }
